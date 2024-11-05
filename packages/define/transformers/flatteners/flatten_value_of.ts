5fcbb7fb2dcb49b6eb7ad2f8518d891e2be3c861
Chris Glover <chris.glover@gmail.com>
2024-11-06 10:52:20 +1100

    flattenValueOf

---
+++ packages/define/transformers/flatteners/flatten_value_of.ts
@@ -0,0 +1,19 @@
+import { type TypeDefHolder } from 'types/definitions'
+import { type FlattenedValueTypesOf } from 'types/flattened_value_types_of'
+import { type ReadonlyTypeDefOf } from 'types/readonly_type_def_of'
+import { type ValueTypeOf } from 'types/value_type_of'
+import {
+  type AnyValueType,
+  flattenValue,
+} from './flatten_value'
+
+function mapper(v: AnyValueType) {
+  return v
+}
+
+export function flattenValueOf<T extends TypeDefHolder>(
+  typeDef: TypeDefHolder,
+  value: ValueTypeOf<ReadonlyTypeDefOf<T>>,
+): FlattenedValueTypesOf<T> {
+  return flattenValue(typeDef, value, mapper)
+}
---
+++ packages/define/transformers/flatteners/specs/flatten_value_of.tests.ts
@@ -0,0 +1,46 @@
+import { flattenValueOf } from 'transformers/flatteners/flatten_value_of'
+import {
+  boolean,
+  list,
+  number,
+  struct,
+} from 'types/builders'
+
+describe('flattenValueOf', function () {
+  // note that we already have tests for the type and the function that this calls, so
+  // this is only a sanity check
+  it('flattens', function () {
+    const flattened = flattenValueOf(
+      struct()
+        .set('a', list(number))
+        .set('b', boolean),
+      {
+        a: [
+          1,
+          2,
+          4,
+        ],
+        b: false,
+      },
+    )
+    expect(flattened).toEqual({
+      $: {
+        a: [
+          1,
+          2,
+          4,
+        ],
+        b: false,
+      },
+      '$.a': [
+        1,
+        2,
+        4,
+      ],
+      '$.a[0]': 1,
+      '$.a[1]': 2,
+      '$.a[2]': 4,
+      '$.b': false,
+    })
+  })
+})
