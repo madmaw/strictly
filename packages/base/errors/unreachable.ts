1671029bec182dab5f82d21246fefcc375407e32
Chris Glover <chris@compassiot.com.au>
2025-06-11 16:02:19 +1000

    weird build error

--- packages/base/errors/unreachable.ts
+++ packages/base/errors/unreachable.ts
@@ -1,4 +1,6 @@
-import { errorHandlingJsonStringify } from 'util/json'
+// absolute import beaks the docs app build
+// eslint-disable-next-line no-relative-import-paths/no-relative-import-paths
+import { errorHandlingJsonStringify } from '../util/json'
 
 export class UnreachableError extends Error {
   constructor(readonly v: never) {
